8c4adb2b9c27317a054accb2471938b46377cce2
Justin Martinez <jlmart88@mit.edu>
2014-10-04 20:05:09 -0400

    Made Storyboard

---
+++ RideShare/RideShare/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RideShare
+//
+//  Created by Justin Martinez on 10/4/14.
+//  Copyright (c) 2014 Justin Martinez. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //_mapView.showsUserLocation = YES;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
